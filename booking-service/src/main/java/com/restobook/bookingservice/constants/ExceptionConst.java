e31a4544f17dcba892ee97625e980eec31e1a44f
Chéridanh <divintsiela@gmail.com>
2025-12-12 10:48:09 +0100

    refactor(booking) : use of constants

--- booking-service/src/main/java/com/restobook/bookingservice/constants/ExceptionConst.java
+++ booking-service/src/main/java/com/restobook/bookingservice/constants/ExceptionConst.java
@@ -6,6 +6,7 @@ public class ExceptionConst {
         throw new IllegalStateException("Utility class");
     }
 
+    public static final String BUSINESS_ERROR = "BUSINESS_ERROR";
     public static final String RESOURCE_NOT_FOUND = "RESOURCE_NOT_FOUND";
     public static final String UNAUTHORIZED = "UNAUTHORIZED";
     public static final String FORBIDDEN = "FORBIDDEN";
